aafccdfffa7f49ccd16b8140fa1c2ab9bf99f3f2
sauhor <sauhor@outlook.com>
2016-03-02 22:28:48 +0900

    Bugfix

--- led.js
+++ led.js
@@ -29,21 +29,18 @@ function blueOn() {
 }
 
 function redOff() {
-    fs.writeFileSync('/sys/class/gpio/export', red_pin);
-    fs.writeFileSync('/sys/class/gpio/gpio'+red_pin+'/direction', 'out');
     fs.writeFileSync('/sys/class/gpio/gpio'+red_pin+'/value', 0);
+    fs.writeFileSync('/sys/class/gpio/unexport', red_pin);
 }
 
 function greenOff() {
-    fs.writeFileSync('/sys/class/gpio/export', green_pin);
-    fs.writeFileSync('/sys/class/gpio/gpio'+green_pin+'/direction', 'out');
     fs.writeFileSync('/sys/class/gpio/gpio'+green_pin+'/value', 0);
+    fs.writeFileSync('/sys/class/gpio/unexport', green_pin);
 }
 
 function blueOff() {
-    fs.writeFileSync('/sys/class/gpio/export', blue_pin);
-    fs.writeFileSync('/sys/class/gpio/gpio'+blue_pin+'/direction', 'out');
     fs.writeFileSync('/sys/class/gpio/gpio'+blue_pin+'/value', 0);
+    fs.writeFileSync('/sys/class/gpio/unexport', blue_pin);
 }
 
 redOn();
