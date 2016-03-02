dcef99018c6cbf0e219f48ec259c04f6c5ead248
sauhor <sauhor@outlook.com>
2016-03-02 22:09:19 +0900

    RGB対応

--- led.js
+++ led.js
@@ -1,13 +1,28 @@
 var fs = require('fs');
 
 var red_pin = 13
- 
-fs.writeFileSync('/sys/class/gpio/export', 26);
-fs.writeFileSync('/sys/class/gpio/gpio26/direction', 'out');
-fs.writeFileSync('/sys/class/gpio/gpio26/value', 1); 
- 
+var green_pin = 19
+var blue_pin = 26
+
+fs.writeFileSync('/sys/class/gpio/export', red_pin);
+fs.writeFileSync('/sys/class/gpio/export', green_pin);
+fs.writeFileSync('/sys/class/gpio/export', blue_pin);
+
+fs.writeFileSync('/sys/class/gpio/gpio'+red_pin+'/direction', 'out');
+fs.writeFileSync('/sys/class/gpio/gpio'+green_pin+'/direction', 'out');
+fs.writeFileSync('/sys/class/gpio/gpio'+blue_pin+'/direction', 'out');
+
+fs.writeFileSync('/sys/class/gpio/gpio'+red_pin+'/value', 1);
+fs.writeFileSync('/sys/class/gpio/gpio'+green_pin+'/value', 1);
+fs.writeFileSync('/sys/class/gpio/gpio'+blue_pin+'/value', 1);
+
+
 setTimeout(function(){
-	fs.writeFileSync('/sys/class/gpio/gpio26/value', 0);
+    fs.writeFileSync('/sys/class/gpio/gpio'+red_pin+'/value', 0);
+    fs.writeFileSync('/sys/class/gpio/gpio'+green_pin+'/value', 0);
+    fs.writeFileSync('/sys/class/gpio/gpio'+blue_pin+'/value', 0);
+	fs.writeFileSync('/sys/class/gpio/unexport', 13);
+	fs.writeFileSync('/sys/class/gpio/unexport', 19);
 	fs.writeFileSync('/sys/class/gpio/unexport', 26);
 }, 2000);
 
